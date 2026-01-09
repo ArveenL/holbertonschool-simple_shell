f89a2f3768c8094577393a2ec5a8369a67e9d8b0
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-09 11:44:44 +0400

    x

--- hsh.c
+++ hsh.c
@@ -4,9 +4,11 @@
 #include <string.h>
 #include <sys/wait.h>
 
+#define MAX_ARGS 64
+
 extern char **environ;
 
-/* checks if line contains only spaces or tabs */
+/* check if line is only spaces or tabs */
 int is_empty(char *s)
 {
 	while (*s)
@@ -18,13 +20,27 @@ int is_empty(char *s)
 	return (1);
 }
 
+/* split line into argv */
+void build_argv(char *line, char **argv)
+{
+	int i = 0;
+	char *token = strtok(line, " \t");
+
+	while (token && i < MAX_ARGS - 1)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, " \t");
+	}
+	argv[i] = NULL;
+}
+
 int main(void)
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
 	pid_t pid;
-	char *argv[2];
+	char *argv[MAX_ARGS];
 	int interactive = isatty(STDIN_FILENO);
 
 	while (1)
@@ -33,7 +49,7 @@ int main(void)
 			write(STDOUT_FILENO, "#cisfun$ ", 9);
 
 		read = getline(&line, &len, stdin);
-		if (read == -1)   /* EOF ONLY */
+		if (read == -1)
 			break;
 
 		if (line[read - 1] == '\n')
@@ -42,12 +58,15 @@ int main(void)
 		if (*line == '\0' || is_empty(line))
 			continue;
 
+		build_argv(line, argv);
+		if (!argv[0])
+			continue;
+
 		pid = fork();
 		if (pid == 0)
 		{
-			argv[0] = line;
-			argv[1] = NULL;
 			execve(argv[0], argv, environ);
+			perror("execve");
 			exit(1);
 		}
 		wait(NULL);
