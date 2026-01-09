c399810851a211dd2b2dadc773e8db9913f4b794
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-09 21:15:05 +0400

    x

--- hsh.c
+++ hsh.c
@@ -9,50 +9,67 @@
 
 extern char **environ;
 
-/* Get PATH manually */
+/* ------------------------- */
+/* Get PATH manually          */
+/* ------------------------- */
 int get_path_env(char *buf, size_t size)
 {
-    int i = 0;
-    while (environ[i])
+    int i, j;
+    for (i = 0; environ[i]; i++)
     {
         if (strncmp(environ[i], "PATH=", 5) == 0)
         {
-            strncpy(buf, environ[i] + 5, size - 1);
-            buf[size - 1] = '\0';
+            for (j = 0; environ[i][5 + j] && j < (int)(size - 1); j++)
+                buf[j] = environ[i][5 + j];
+            buf[j] = '\0';
             return 1;
         }
-        i++;
     }
     return 0;
 }
 
+/* ------------------------- */
 /* Check if file is executable */
+/* ------------------------- */
 int is_executable(const char *path)
 {
     return access(path, X_OK) == 0;
 }
 
-/* Concatenate dir + "/" + cmd into full_path */
+/* ------------------------- */
+/* Build full path: dir/cmd   */
+/* ------------------------- */
 void build_path(char *dest, const char *dir, const char *cmd)
 {
-    size_t len = 0;
+    int i = 0, j = 0;
 
-    while (*dir)
-        dest[len++] = *dir++;
-    dest[len++] = '/';
-    while (*cmd)
-        dest[len++] = *cmd++;
-    dest[len] = '\0';
+    while (dir[i])
+    {
+        dest[i] = dir[i];
+        i++;
+    }
+    dest[i++] = '/';
+
+    while (cmd[j])
+    {
+        dest[i] = cmd[j];
+        i++;
+        j++;
+    }
+    dest[i] = '\0';
 }
 
-/* Find command in PATH */
+/* ------------------------- */
+/* Find command in PATH       */
+/* ------------------------- */
 int find_command(char *cmd, char *full_path, size_t size)
 {
     char path_env[MAX_PATH];
-    char *dir;
     char tmp[MAX_PATH];
+    char *dir;
 
-    if (strchr(cmd, '/')) /* absolute or relative path */
+    /* If command contains /, treat as path */
+    if (strchr(cmd, '/'))
     {
         if (is_executable(cmd))
         {
@@ -63,9 +80,11 @@ int find_command(char *cmd, char *full_path, size_t size)
         return 0;
     }
 
+    /* Get PATH manually */
     if (!get_path_env(path_env, sizeof(path_env)))
         return 0;
 
+    /* Iterate directories in PATH */
     dir = strtok(path_env, ":");
     while (dir)
     {
@@ -78,23 +97,31 @@ int find_command(char *cmd, char *full_path, size_t size)
         }
         dir = strtok(NULL, ":");
     }
+
     return 0;
 }
 
-/* Parse line into arguments */
+/* ------------------------- */
+/* Parse line into argv array */
+/* ------------------------- */
 int parse_line(char *line, char **argv)
 {
     int argc = 0;
     char *token = strtok(line, " \t\n");
+
     while (token && argc < MAX_ARGS - 1)
     {
         argv[argc++] = token;
         token = strtok(NULL, " \t\n");
     }
+
     argv[argc] = NULL;
     return argc;
 }
 
+/* ------------------------- */
+/* Main shell loop            */
+/* ------------------------- */
 int main(void)
 {
     char line[MAX_LINE];
@@ -120,12 +147,14 @@ int main(void)
         if (parse_line(line, argv) == 0)
             continue;
 
+        /* Find command in PATH */
         if (!find_command(argv[0], full_path, sizeof(full_path)))
         {
             write(2, "command not found\n", 18);
             continue;
         }
 
+        /* Fork only if command exists */
         pid = fork();
         if (pid < 0)
             exit(1);
@@ -141,6 +170,7 @@ int main(void)
             waitpid(pid, &status, 0);
         }
     }
+
     return 0;
 }
 
